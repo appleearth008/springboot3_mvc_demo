fd1982d53760d18d7b79cd82a3827ba08b996d88
apple008 <89285711+appleearth008@users.noreply.github.com>
2023-03-15 19:48:00 -0700

    initial commit

---
+++ src/test/java/com/example/thymeleafMVC/ThymeleafMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thymeleafMVC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
